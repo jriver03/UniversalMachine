b15cc45cc1c7ae42027dca917e105c33ad211c24
Joshua Rivera <jriver44@gmail.com>
2025-08-24 09:43:08 -0600

    fixed some small issues and a bug in disasm.c

--- src/disasm.c
+++ src/disasm.c
@@ -1,5 +1,11 @@
+#ifndef _POSIX_C_SOURCE
 #define _POSIX_C_SOURCE 2000809L
+#endif
+
+#ifndef _FILE_OFFSET_BITS
 #define _FILE_OFFSET_BITS 64
+#endif
+
 #include <sys/types.h>
 
 #include <stdio.h>
@@ -10,5 +16,6 @@
 
 static void die(const char *msg) {
     fprintf(stderr, "disasm: %s\n", msg);
+    exit(1);
 }
 
